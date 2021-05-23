e2e1e12946c133f8b3669b0fdea0fff5e1160a07
unknown <kratenku@gmail.com>
2021-05-22 22:39:33 -0500

    “Hola”

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VistaComponent } from './vista/vista.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { ModeloComponent } from './modelo/modelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaComponent,
+    CarruselComponent,
+    ModeloComponent
   ],
   imports: [
     BrowserModule
