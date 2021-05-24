f649ca25ce38cb689ed79aa57a1399c502ad6657
unknown <kratenku@gmail.com>
2021-05-23 22:48:57 -0500

    Actualizacion mapa

---
+++ src/app/modelo/control.js
@@ -0,0 +1,6 @@
+var mapa;
+        function getmapa(){
+          mapa= new google.maps.Map(document.getElementById('mapa'),{
+            center: {lat:3.4048576,lng:-76.5155485},zoom: 18
+          });
+        }
